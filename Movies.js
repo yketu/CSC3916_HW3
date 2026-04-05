02f0a806a981e67f6d78a844e06fa2f5b6361f7e
yketu <157855738+yketu@users.noreply.github.com>
2026-04-04 20:29:24 -0600

    Refactor MovieSchema to use mongoose.Schema

--- Movies.js
+++ Movies.js
@@ -14,8 +14,19 @@ const connectDB = async () => {
 connectDB();
 
 // Movie schema
-var MovieSchema = new Schema({
-
+const MovieSchema = new mongoose.Schema({
+  title: { type: String, required: true, index: true },
+  releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
+  genre: {
+    type: String,
+    enum: [
+      'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+    ],
+  },
+  actors: [{
+    actorName: String,
+    characterName: String,
+  }],
 });
 
 module.exports = mongoose.model('Movie', MovieSchema);
